3bb13ade25e19c39fbfea5c69bf847191f7fa064
Tobias Bocanegra <tripod@adobe.com>
2022-04-11 14:31:42 +0200

    fix: fix release

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -10,6 +10,9 @@ module.exports = {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
+    ['@semantic-release/exec', {
+      publishCmd: 'npm run deploy',
+    }],
     ["@semantic-release/github", {}]
   ],
   branches: ['main'],
